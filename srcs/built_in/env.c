f2b648a7ddf695462d74599b0c138cae840c1bb7
Thiwanka Somachandra <thivankas@outlook.com>
2025-04-24 06:18:16 +0300

    Re-arrange the code to comply with norminette stds

--- srcs/built_in/env.c
+++ srcs/built_in/env.c
@@ -6,8 +6,28 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 04:48:22 by tsomacha          #+#    #+#             */
-/*   Updated: 2025/04/24 04:48:26 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/04/24 04:51:27 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shell.h"
+
+int	builtin_env(t_shell *mini)
+{
+	t_env	*temp;
+
+	temp = mini->initenv->env;
+	if (!mini->cmds->args[1])
+	{
+		while (temp)
+		{
+			printf("%s", temp->name);
+			printf("=");
+			printf("%s\n", temp->value);
+			temp = temp->next;
+		}
+		return (0);
+	}
+	else
+		return (1);
+}
