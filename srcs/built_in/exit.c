6d9501bf401d1d53701c4cd869b5ce15bf6e6dc7
Thiwanka Somachandra <thivankas@outlook.com>
2025-04-25 05:31:33 +0300

    Re-arrange code to comply with norminette stds

--- srcs/built_in/exit.c
+++ srcs/built_in/exit.c
@@ -6,8 +6,15 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 04:48:30 by tsomacha          #+#    #+#             */
-/*   Updated: 2025/04/24 04:48:33 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/04/25 05:30:12 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shell.h"
+
+int	builtin_exit(t_shell *mini)
+{
+	printf("exit\n");
+	clear_and_exit(mini);
+	exit (mini->status);
+}
