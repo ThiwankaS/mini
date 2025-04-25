0acb950d6fd33d05844503fa8e5cabda3c9dd0e2
Thiwanka Somachandra <thivankas@outlook.com>
2025-04-25 05:26:19 +0300

    Re-arrange code to comply with norminette stds

--- srcs/built_in/unset.c
+++ srcs/built_in/unset.c
@@ -6,8 +6,47 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 04:48:57 by tsomacha          #+#    #+#             */
-/*   Updated: 2025/04/24 04:49:01 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/04/25 05:25:58 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shell.h"
+
+static void	remove_env_nodes(t_shell *mini, char *unset)
+{
+	t_env	*current;
+	t_env	*prev;
+
+	current = mini->initenv->env;
+	prev = NULL;
+	if (!unset)
+		return ;
+	while (current)
+	{
+		if (ft_strncmp(current->name, unset, ft_strlen(unset)) == 0)
+		{
+			if (prev)
+				prev->next = current->next;
+			else
+				mini->initenv->env = current->next;
+			free(current->name);
+			if (current->value)
+				free(current->value);
+			free(current);
+			return ;
+		}
+		prev = current;
+		current = current->next;
+	}
+}
+
+void	builtin_unset(t_shell *mini, char **unset)
+{
+	int	i;
+
+	i = 1;
+	if (!unset)
+		return ;
+	while (unset[i])
+		remove_env_nodes(mini, unset[i++]);
+}
