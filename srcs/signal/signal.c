97a308cb86e777477e902327d65dbb7b313da394
Thiwanka Somachandra <thivankas@outlook.com>
2025-04-29 03:32:27 +0300

    Implementing SIGQUIT handling

--- srcs/signal/signal.c
+++ srcs/signal/signal.c
@@ -6,15 +6,15 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 22:00:35 by tsomacha          #+#    #+#             */
-/*   Updated: 2025/04/23 22:00:38 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/04/29 03:26:54 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/shell.h"
 
-void handler(int sig)
+void	handle_int(int sig)
 {
-	if(sig == SIGINT)
+	if (sig == SIGINT)
 	{
 		rl_replace_line("", 0);
 		printf("\n");
@@ -23,7 +23,16 @@ void handler(int sig)
 	}
 }
 
-void init_sig(void)
+void	handle_quit(int sig)
 {
-	signal(SIGINT, handler);
+	if (sig == SIGQUIT)
+	{
+		(void)sig;
+	}
+}
+
+void	init_sig(void)
+{
+	signal(SIGINT, handle_int);
+	signal(SIGQUIT, handle_int);
 }
