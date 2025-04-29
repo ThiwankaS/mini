3e3442900f4beb8c23a04d568de823f8eda622c5
Thiwanka Somachandra <thivankas@outlook.com>
2025-04-29 04:18:44 +0300

    Isolate quoted command handling from qote validating

---
+++ srcs/parse/surround.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   surround.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 04:06:53 by tsomacha          #+#    #+#             */
+/*   Updated: 2025/04/29 04:07:32 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/shell.h"
+
+t_cmd *handle_quoted(t_shell *mini, char *token)
+{
+	t_cmd *cmd = malloc(sizeof(t_cmd));
+	if(!cmd)
+		return (NULL);
+	cmd->cmd = get_command(token);
+	cmd->type = SMPL_CMD;
+	cmd->command = set_path_name(mini, token);
+	cmd->num_args = 2;
+	cmd->filename = NULL;
+	cmd->args = set_arg_array(token, cmd->num_args);
+	cmd->next = NULL;
+	return (cmd);
+}
